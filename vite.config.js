f752701b8fb75368aaca2bf89883548e0c65967e
hsanjay <1ds22cy017@dsce.edu.in>
2025-03-10 07:31:37 +0530

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_API_URL, // Backend URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+        '/pay': {
+          target: 'http://localhost:9090',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/pay/, ''),
+        },
       },
     },
-  },
+  };
 });
